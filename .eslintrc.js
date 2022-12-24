2ce0b547046ce948bb477369e808064905edcf75
Azamat Zulpykhar <augustintherome@gmail.com>
2022-12-24 18:34:02 +0600

    Cleanup and eslint setup

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+const { configure, presets } = require("eslint-kit");
+
+module.exports = configure({
+  root: __dirname,
+  presets: [
+    presets.node(),
+    presets.react(),
+    presets.nextJs(),
+    presets.prettier(),
+    presets.imports(),
+  ],
+});
