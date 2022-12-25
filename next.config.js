5b8834c93871e3108077de528d0b1717d8260918
Azamat Zulpykhar <augustintherome@gmail.com>
2022-12-25 09:11:26 +0600

    Redirect

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "https://video.amazat.xyz",
+        destination:
+          "https://www.loom.com/share/e773bfff865c4c9aa47f9daff4e52e44",
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
