3136a981015f1d3184bcb7560b5e1d664aa659df
Azamat Zulpykhar <augustintherome@gmail.com>
2022-12-25 09:19:20 +0600

    Fix

--- next.config.js
+++ next.config.js
@@ -4,10 +4,12 @@ const nextConfig = {
     return [
       {
         source: "/:path*",
-        has: {
-          type: "host",
-          value: "video.amazat.xyz",
-        },
+        has: [
+          {
+            type: "host",
+            value: "video.amazat.xyz",
+          },
+        ],
         destination:
           "https://www.loom.com/share/e773bfff865c4c9aa47f9daff4e52e44",
         permanent: true,
