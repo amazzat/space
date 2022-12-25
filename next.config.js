0934b346b28a1c2f17c9fdfebd85f84c021f53b3
Azamat Zulpykhar <augustintherome@gmail.com>
2022-12-25 09:18:17 +0600

    Fix

--- next.config.js
+++ next.config.js
@@ -3,7 +3,11 @@ const nextConfig = {
   async redirects() {
     return [
       {
-        source: "https://video.amazat.xyz",
+        source: "/:path*",
+        has: {
+          type: "host",
+          value: "video.amazat.xyz",
+        },
         destination:
           "https://www.loom.com/share/e773bfff865c4c9aa47f9daff4e52e44",
         permanent: true,
